a6f4317e319fad174adada11e1f319b9ddcd3c77
Sheila R Moore <sherexmykes@github.com>
2020-09-11 00:07:53 -0400

    day 4 cpu

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,6 +1,9 @@
 """CPU functionality."""
 
+
 import sys
+
+filename = sys.argv[1]
 
 
 class CPU:
@@ -8,10 +11,23 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.reg = [0]*8
-        self.reg[7] = 0xF4
-        self.ram = [0]*256
+        self.ram = [0] * 256
         self.pc = 0
+        self.register = [0] * 8
+        self.running = True
+        self.SP = 0xf4
+
+        self.branch_table = {
+            0b10000010: self.LDI,
+            0b01000111: self.PRN,
+            0b00000001: self.HLT,
+            0b10100000: self.ADD,
+            0b10100010: self.MUL,
+            0b01000101: self.PUSH,
+            0b01000110: self.POP,
+            0b00010001: self.RET,
+            0b01010000: self.CALL
+        }
 
     def load(self):
         """Load a program into memory."""
@@ -28,13 +44,13 @@ class CPU:
         #     0b00000001,  # HLT
         # ]*8
 
-        if len(sys.argv) < 2:
-            print('please pass in a second filename')
-            sys.exit()
-        try:
-            address = 0
-            with open(sys.argv[1]) as files:
-                for line in files:
+        #if len(sys.argv) < 2:
+            #print('please pass in a second filename')
+            #sys.exit()
+        #try:
+        address = 0
+        with open(sys.argv[1]) as files:
+            for line in files:
                     split_line = line.split('#')
                     command = split_line[0].strip()
                     if command == '':
@@ -43,28 +59,19 @@ class CPU:
 
                     self.ram[address] = num_command
                     address += 1
-        except FileNotFoundError:
-            print(f'{sys.argv[0]}{sys.argv[1]}file was not found')
-            sys.exit()
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.register[reg_a] += self.register[reg_b]
         # elif op == "SUB": etc
+        elif op == "MUL":
+           # print("reg", self.register[reg_a])
+            self.register[reg_a] *= self.register[reg_b]
+            # print(self.register[reg_a])
         else:
             raise Exception("Unsupported ALU operation")
-
-    def ram_read(self, address):
-        return self.ram[address]
-
-    def ram_write(self, address, value):
-        self.reg[address] = value
 
     def trace(self):
         """
@@ -79,56 +86,80 @@ class CPU:
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
+
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.register[i], end='')
 
         print()
 
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
+
+    def HLT(self):
+        self.running = False
+
+    def LDI(self):
+        address = self.ram_read(self.pc + 1)
+        value = self.ram_read(self.pc + 2)
+        self.register[address] = value
+
+    def PRN(self):
+        address = self.ram_read(self.pc + 1)
+        value = self.register[address]
+        print(value)
+
+    def ADD(self):
+        operand1 = self.ram_read(self.pc + 1)
+        operand2 = self.ram_read(self.pc + 2)
+        self.alu('ADD', operand1, operand2)
+
+    def MUL(self):
+        operand1 = self.ram_read(self.pc + 1)
+        operand2 = self.ram_read(self.pc + 2)
+        self.alu('MUL', operand1, operand2)
+
+    def PUSH(self):
+        self.SP -= 1
+        address = self.ram[self.pc + 1]
+        value = self.register[address]
+        self.ram[self.SP] = value
+
+    def POP(self):
+        address = self.ram[self.pc + 1]
+        value = self.ram[self.SP]
+        self.register[address] = value
+        self.SP += 1
+
+    def CALL(self):
+        self.SP -= 1
+        ret_address = self.pc + 2
+        self.ram[self.SP] = ret_address
+        reg = self.ram[self.pc + 1]
+        self.pc = self.register[reg]
+
+    def RET(self):
+        ret_address = self.ram[self.SP]
+        self.pc = ret_address
+        self.SP += 1
+
     def run(self):
         """Run the CPU."""
-        self.load()
-        while self.pc < len(self.ram):
-            command = self.ram[self.pc]
-            HLT = 0b00000001
-            operand1 = self.ram_read(self.pc+1)
-            operand2 = self.ram_read(self.pc+2)
 
-            if command == HLT:  # stops program
-                break
+        while self.running:
+            IR = self.ram_read(self.pc)
+            if IR in self.branch_table:
+                self.branch_table[IR]()
+                operands = (IR & 0b11000000) >> 6
+                param = (IR & 0b00010000) >> 4
 
-            if command == 0b10000010:  # LDI/Save
-                # registers the next line as the index inserting the line after that one as the value
-                self.ram_write(operand1, operand2)
+                if not param:
+                    self.pc += operands + 1
 
-            if command == 0b01000111:  # prints next line
-                print(self.reg[operand1])
-
-            if command == 0b10100010:  # multiplies the numbers of the indexes of the next 2 lines
-                self.reg[operand1] *= self.reg[operand2]
-
-            if command == 0b01000101:  # Push
-                self.reg[7] -= 1  # decrement stack pointer
-                self.reg[7] &= 0xff
-
-                # get the index
-                reg_index = self.ram[self.pc+1]
-                # get the value at the pointer's address
-                value = self.reg[reg_index]
-                self.ram[self.reg[7]] = value
-
-            if command == 0b01000110:  # Pop
-                # get the stack pointer
-                sp = self.reg[7]
-                # get register number to put value in
-                reg = self.ram[self.pc+1]
-                # use stack pointer to get the value
-                value = self.ram[sp]
-                # put the value into the given register
-                self.reg[reg] = value
-
-                self.reg[7] += 1
-
-            self.pc += command >> 6
-            self.pc += 1
+            else:
+                print(f"Can't find {IR} with index of {self.pc}")
+                sys.exit(1)
