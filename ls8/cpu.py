06f4ef60c040f51792ca4e0a7bca78098490744a
Sheila R Moore <sherexmykes@github.com>
2020-09-11 21:47:25 -0400

    sprint

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,51 +3,47 @@
 
 import sys
 
-filename = sys.argv[1]
-
+# Instructions
+LDI = 0b10000010
+CMP = 0b10100111
+JEQ = 0b01010101
+PRN = 0b01000111
+JNE = 0b01010110
+HLT = 0b00000001
+JMP = 0b01010100
 
 class CPU:
-    """Main CPU class."""
+  
 
     def __init__(self):
-        """Construct a new CPU."""
+       
         self.ram = [0] * 256
+        self.registers = [0] * 8
+        self.registers[7] = 0xF4
         self.pc = 0
-        self.register = [0] * 8
         self.running = True
-        self.SP = 0xf4
-
-        self.branch_table = {
-            0b10000010: self.LDI,
-            0b01000111: self.PRN,
-            0b00000001: self.HLT,
-            0b10100000: self.ADD,
-            0b10100010: self.MUL,
-            0b01000101: self.PUSH,
-            0b01000110: self.POP,
-            0b00010001: self.RET,
-            0b01010000: self.CALL
-        }
+        self.FL = 0b00000000
+        self.LDI = 0b10000010
+        self.CMP = 0b10100111
+        self.JEQ =  0b01010101
+        self.JNE = 0b01010110
+        self.JMP = 0b01010100
+        self.PRN =  0b01000111
+        self.HLT = 0b00000001
+        
+        #self.branch_table = {
+            #0b10000010: self.LDI,
+            #0b10100111: self.CMP,
+            #0b01010101: self.JEQ,
+            #0b01010110: self.JNE,
+            #0b01010100: self.JMP,
+            #0b01000111: self.PRN,
+            #0b00000001: self.HLT,
+           
+       # }
 
     def load(self):
-        """Load a program into memory."""
 
-        # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010,  # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111,  # PRN R0
-        #     0b00000000,
-        #     0b00000001,  # HLT
-        # ]*8
-
-        #if len(sys.argv) < 2:
-            #print('please pass in a second filename')
-            #sys.exit()
-        #try:
         address = 0
         with open(sys.argv[1]) as files:
             for line in files:
@@ -60,106 +56,99 @@ class CPU:
                     self.ram[address] = num_command
                     address += 1
 
-    def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
+    def alu(self, instruction, register1, register2):
+        if instruction == "CMP":
+            self.FL &= 0b00000000
+            if self.registers[register1] == self.registers[register2]:
+                self.FL = 0b00000001
+            elif self.registers[register1] < self.registers[register2]:
+                self.FL = 0b00000100
+            elif self.registers[register1] > self.registers[register2]:
+                self.FL = 0b00000010
+            self.pc += 3
 
-        if op == "ADD":
-            self.register[reg_a] += self.register[reg_b]
-        # elif op == "SUB": etc
-        elif op == "MUL":
-           # print("reg", self.register[reg_a])
-            self.register[reg_a] *= self.register[reg_b]
-            # print(self.register[reg_a])
-        else:
-            raise Exception("Unsupported ALU operation")
-
-    def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
-
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            # self.fl,
-            # self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-
-        ), end='')
-
-        for i in range(8):
-            print(" %02X" % self.register[i], end='')
-
-        print()
-
-    def ram_read(self, address):
-        return self.ram[address]
-
-    def ram_write(self, value, address):
-        self.ram[address] = value
-
-    def HLT(self):
+    def hlt(self):
         self.running = False
 
-    def LDI(self):
-        address = self.ram_read(self.pc + 1)
-        value = self.ram_read(self.pc + 2)
-        self.register[address] = value
+    def jeq(self, register_number):
+        if self.FL & 1 == 1:
+            self.pc = self.registers[register_number]
+        else:
+            self.pc += 2
 
-    def PRN(self):
-        address = self.ram_read(self.pc + 1)
-        value = self.register[address]
-        print(value)
+    def jmp(self, register_number):
+        self.pc = self.registers[register_number]
 
-    def ADD(self):
-        operand1 = self.ram_read(self.pc + 1)
-        operand2 = self.ram_read(self.pc + 2)
-        self.alu('ADD', operand1, operand2)
+    def jne(self, register_number):
+        if self.FL & 1 == 0:
+            self.pc = self.registers[register_number]
+        else:
+            self.pc += 2
 
-    def MUL(self):
-        operand1 = self.ram_read(self.pc + 1)
-        operand2 = self.ram_read(self.pc + 2)
-        self.alu('MUL', operand1, operand2)
+    def ldi(self, register_number, value):
+        self.registers[register_number] = value
+        self.pc += 3
 
-    def PUSH(self):
-        self.SP -= 1
-        address = self.ram[self.pc + 1]
-        value = self.register[address]
-        self.ram[self.SP] = value
+    def prn(self, register_number):
+        print(self.registers[register_number])
+        self.pc += 2
 
-    def POP(self):
-        address = self.ram[self.pc + 1]
-        value = self.ram[self.SP]
-        self.register[address] = value
-        self.SP += 1
+    def load(self):
+        if len(sys.argv) < 2:
+            print("Please pass in a second file name: python3 ls8.py second_filename.py")
+            sys.exit()
+        file_name = sys.argv[1]
+        try:
+            address = 0
+            with open(file_name) as file:
+                for line in file:
+                    split_line = line.split('#')[0]
+                    command = split_line.strip()
 
-    def CALL(self):
-        self.SP -= 1
-        ret_address = self.pc + 2
-        self.ram[self.SP] = ret_address
-        reg = self.ram[self.pc + 1]
-        self.pc = self.register[reg]
+                    if command == '':
+                        continue
 
-    def RET(self):
-        ret_address = self.ram[self.SP]
-        self.pc = ret_address
-        self.SP += 1
+                    instruction = int(command, 2)
+                    self.ram[address] = instruction
+                    address += 1
+        except FileNotFoundError:
+            print(f'{sys.argv[0]}: {sys.argv[1]} file was not found')
+            sys.exit()
 
     def run(self):
-        """Run the CPU."""
-
+        self.load()
         while self.running:
-            IR = self.ram_read(self.pc)
-            if IR in self.branch_table:
-                self.branch_table[IR]()
-                operands = (IR & 0b11000000) >> 6
-                param = (IR & 0b00010000) >> 4
+            instruction = self.ram[self.pc]
 
-                if not param:
-                    self.pc += operands + 1
+            if instruction == CMP:
+                register1 = self.ram[self.pc + 1]
+                register2 = self.ram[self.pc + 2]
+                self.alu("CMP", register1, register2)
+
+            elif instruction == HLT:
+                self.hlt()
+
+            elif instruction == JEQ:
+                register_number = self.ram[self.pc + 1]
+                self.jeq(register_number)
+
+            elif instruction == JMP:
+                register_number = self.ram[self.pc + 1]
+                self.jmp(register_number)
+
+            elif instruction == JNE:
+                register_number = self.ram[self.pc + 1]
+                self.jne(register_number)
+
+            elif instruction == LDI:
+                register_number = self.ram[self.pc + 1]
+                value = self.ram[self.pc + 2]
+                self.ldi(register_number, value)
+
+            elif instruction == PRN:
+                register_number = self.ram[self.pc + 1]
+                self.prn(register_number)
 
             else:
-                print(f"Can't find {IR} with index of {self.pc}")
-                sys.exit(1)
+                print(f"Instruction number {self.pc} not recognized!")
+                self.pc += 1
